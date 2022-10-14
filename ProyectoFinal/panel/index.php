f30c00789e3979c745834ae774424eddc870f77f
JuanLD89 <62732117+JuanLD89@users.noreply.github.com>
2022-10-14 12:34:00 -0500

    enlace

--- ProyectoFinal/panel/index.php
+++ ProyectoFinal/panel/index.php
@@ -27,7 +27,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Consolas Retro</a>
+          <a class="navbar-brand" href="../index.php">Consolas Retro</a>
         </div>
         
       </div>
